59903413b1e1b419fc6ee1d403c77ec4f28b154e
archanajss <archanajss@gmail.com>
2022-05-27 02:01:23 +0200

    refining sqs and tests

--- task2/tests/messages_test.py
+++ task2/tests/messages_test.py
@@ -1,35 +1,39 @@
 import dataclasses
-from json import dumps
+from json import dumps, loads
 from pprint import pprint
+from unicodedata import category
 from unittest import TestCase
 
 from models.car import Car
-from utils.sqs_helper import delete_message
-from utils.sqs_helper import purge_queue
-from utils.sqs_helper import produce_message
+from utils.sqs import receive_messages
+from utils.sqs import send_message
 
 class SQSMessageTest(TestCase):
-    
-    @classmethod
-    def setup_class(cls):
-        purge_queue()
 
-    @classmethod
-    def teardown_class(cls):
-        purge_queue()
+    def test_send_messages(self):
+        # produce messages on sqs
+        self.__add_cars_to_queue()
+        # consume messages
+        response = receive_messages()
+        messages = response.get('Messages', [])
+        for i,message in enumerate(messages):
+            car = loads(message['Body'])
+            self.assertEqual(car['brand_name'], f'brand{str(i+1)}')
 
-    def test_produce_messages(self):
-        car = Car(brand_name='bmw', model='2 Series', number_of_doors=5, sports_car=False)
-        json_car = dumps(dataclasses.asdict(car))
-        response = produce_message(json_car)
-        pprint(response)
-        message_id = response['MessageId']
-        message = self.get_message_by_id(messages=response, message_id=message_id)
-        self.assertTrue(message)
-        pprint(message['Body'])
-        delete_message(message_id)
+    def __add_cars_to_queue(self):
+        for i in range(1, 3):
+            car = Car(
+                brand_name=f'brand{str(i)}', 
+                model=f'model {str(i)}', 
+                number_of_doors=4, 
+                sports_car=False
+            )
+            json_car = dumps(dataclasses.asdict(car))
+            response = send_message(json_car)
+            pprint(response)
 
     def get_message_by_id(messages, message_id):
         for message in messages:
+            print(message['MessageId'])
             if message['MessageId'] == message_id:
                 return message
---
+++ task2/utils/sqs.py
@@ -0,0 +1,44 @@
+from pprint import pprint
+import boto3
+
+from config import ENDPOINT_URL
+
+def send_message(msg):
+    pprint(msg)
+    sqs_client = boto3.client('sqs', aws_access_key_id='secret_id', aws_secret_access_key='secret_key', endpoint_url=ENDPOINT_URL)
+    response = sqs_client.send_message(
+        QueueUrl=get_queue_url(sqs_client),
+        MessageBody=(msg)
+    )
+    pprint(response)
+    return response
+
+def receive_messages():
+    sqs_client = boto3.client('sqs', aws_access_key_id='secret_id', aws_secret_access_key='secret_key', endpoint_url=ENDPOINT_URL)
+    messages = sqs_client.receive_message(
+        QueueUrl=get_queue_url(sqs_client),
+        MaxNumberOfMessages=10,
+        WaitTimeSeconds=10
+    )
+    pprint(messages)
+    return messages
+
+def delete_message(receipt_handle):
+    sqs_client = boto3.client('sqs', aws_access_key_id='secret_id', aws_secret_access_key='secret_key', endpoint_url=ENDPOINT_URL)
+    response = sqs_client.client.delete_message(
+        QueueUrl=get_queue_url(sqs_client),
+        ReceiptHandle=receipt_handle
+    )
+    pprint(response)
+
+def get_queue_url(sqs_client):
+    response = sqs_client.get_queue_url(
+        QueueName='cars'
+    )
+    return response['QueueUrl']
+
+def purge_queue():
+    sqs_client = boto3.client('sqs', aws_access_key_id='secret_id', aws_secret_access_key='secret_key', endpoint_url=ENDPOINT_URL)
+    sqs_client.purge_queue(
+        QueueUrl=get_queue_url(sqs_client)
+    )
